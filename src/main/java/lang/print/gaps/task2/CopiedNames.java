d0747293f6b357431f5dffb40a00cad21e6680eb
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-19 14:32:59 +0400

    fix quality lol

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,10 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int firstValue = 1000;
-        int secondValue = 3000;
+        int variableOne = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
         int variables = 5000;
         variables = 7000;
+        int valueThree = 90000;
     }
 }
